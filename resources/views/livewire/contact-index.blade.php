b8843d959d94237d5b47f6b6a6dab3120f8d704f
rhaikal <rhaikal101@gmail.com>
2022-08-02 19:40:47 +0700

    menampilkan data contact dalam table

--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -1,5 +1,25 @@
 <div>
-    @foreach ($contacts as $contact)
-        <li>{{ $contact->name }}</li>
-    @endforeach
+    <table class="table">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($contacts as $contact)
+                <tr>
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td>{{ $contact->name }}</td>
+                    <td>{{ $contact->phone }}</td>
+                    <td>
+                        <button class="btn btn-sm btn-info text-white">Edit</button>
+                        <button class="btn btn-sm btn-danger text-white">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
